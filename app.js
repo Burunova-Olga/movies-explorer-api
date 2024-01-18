bae5ade2639970b1b0d4676fe08ef72f2a80ef5d
Burunova Olga <burun-olya@yandex.ru>
2024-01-18 22:48:48 +0300

    rate limiter

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const { errors } = require('celebrate');
 const auth = require('./middlewares/auth');
 const NotFoundError = require('./errors/not-found-error');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { limiter } = require('./middlewares/express-rate-limit');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 const app = express();
@@ -17,6 +18,7 @@ mongoose.connect(DB_URL)
 
 app.use(requestLogger);
 app.use(helmet());
+app.use(limiter);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
