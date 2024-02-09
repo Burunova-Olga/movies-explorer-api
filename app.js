4d302e474e92f89ac60f5b2fc5de5dc3414c7bcd
Burunova Olga <burun-olya@yandex.ru>
2024-02-09 23:46:01 +0300

    grammatic error

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const helmet = require('helmet');
+const  = require('helmet');
 const { errors } = require('celebrate');
 const auth = require('./middlewares/auth');
 const NotFoundError = require('./errors/not-found-error');
