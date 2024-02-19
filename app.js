eec45c9256ea9573cdeef019d07f1b401d84dad7
Burunova Olga <burun-olya@yandex.ru>
2024-02-19 23:34:04 +0300

    debug cors

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const { limiter } = require('./middlewares/express-rate-limit');
 
 const cors = require('cors');
 
-const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 const app = express();
 
 mongoose.connect(DB_URL)
